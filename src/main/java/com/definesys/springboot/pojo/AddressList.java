1ca35e6713d2b45fc1670b574639c1b9ba8a0b61
Souu2020 <1097679666@qq.com>
2020-07-30 11:35:04 +0800

    新增了AddressList.java

---
+++ src/main/java/com/definesys/springboot/pojo/AddressList.java
@@ -0,0 +1,77 @@
+package com.definesys.springboot.pojo;
+
+
+public class AddressList {
+
+  private String userid;
+  private String username;
+  private String userpwd;
+  private String phone;
+  private String email;
+  private String address;
+  private String mtext;
+
+
+  public String getUserid() {
+    return userid;
+  }
+
+  public void setUserid(String userid) {
+    this.userid = userid;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public String getUserpwd() {
+    return userpwd;
+  }
+
+  public void setUserpwd(String userpwd) {
+    this.userpwd = userpwd;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  public String getMtext() {
+    return mtext;
+  }
+
+  public void setMtext(String mtext) {
+    this.mtext = mtext;
+  }
+
+}
